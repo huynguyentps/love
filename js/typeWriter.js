60d2a732b6b085baf23c2077106e4a371cb36754
nguyen quang huy <huy.nguyen@tpssoft.com>
2023-09-11 17:50:23 +0700

    test

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "hello word";
+let text2 = "đây là một dòng test"
 let speed = 100;
 
 function typeWriter(text, para){
