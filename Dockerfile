6ba37125585a54e6577fded24bfa133abd453536
nguyen quang huy <huy.nguyen@tpssoft.com>
2023-09-13 17:41:10 +0700

    update

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM nginx:1.18.0
-COPY admin-devops-dev.conf /etc/nginx/conf.d
-# COPY /index.html /usr/share/nginx/html
-# COPY /CNAME /usr/share/nginx/html
-# ADD css /usr/share/nginx/html/css
-# ADD cards /usr/share/nginx/html/cards
-# ADD cardback.JPG /usr/share/nginx/html/cardback.JPG
-# ADD package*.json /usr/share/nginx/html
-COPY . /usr/share/nginx/html
---
+++ config.js
@@ -0,0 +1,22 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Gửi em bé Nguyễn Ngọc Tú Anh của anh nè!!!",
+        "Hôm qua tới giờ làm em hong vui",
+        "Anh Tiêu xin lỗi bé Muối nhiều nè",
+        "Anh luôn ở bên em mọi lúc đó dù có ở xa nhưng mà có thể mang được em đi mọi nơi nữa đóa em biết làm sao được hong",
+        "Vì trái tim của em ở trong anh rồi đây nè ^^",
+        "Giữ cả đời đó biết chưa Huy?",
+        "Yêu em Muối <3",
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2023-07-28T20:15:00",
+    // 双方的名字
+    names: ["Muối", "Tiêu"]
+}
